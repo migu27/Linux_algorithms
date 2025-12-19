a98b066c59cdc3d5f7024fa4e418ee30468b316d
migu27 <migu27@gmail.com>
2025-12-19 05:30:35 +0100

    Add files via upload

--- Square_root.cpp
+++ Square_root.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int NUM_STEPS = 50;
+const int NUM_STEPS = 10;
 
 // Small piece of code moved to a function to keep the algorithm's code clean
 void print(double val) {
@@ -13,14 +13,12 @@ void print(double val) {
 	     << "\t\t Trying... " << val << endl;
 }
 
-// Find the first value (left side of the interval) to start the iterations
-int find_first_value(int target) {
-	int result, tmp = 0;
-	while (result * result < target) ++result;
-	return (result-1);
-}
+double square_root_by_binary_search(int target) {
+	// Find the first value (left side of the interval) to start the iterations
+	int first_value = 0;
+	while (first_value * first_value < target) ++first_value;
+	first_value -=1 ; // We want the left side of the interval
 
-double square_root(int first_value, int target) {
 	// Build the interval, defined by a left and a right value
 	double left = (double)first_value;
 	double right = left + 1.0;
@@ -44,6 +42,22 @@ double square_root(int first_value, int target) {
 	return result;
 }
 
+double square_root_by_newton(int target) {
+	// Find the first value (right side of the interval) to start the iterations
+        int first_value = 0;
+        while (first_value * first_value < target) ++first_value;
+
+	double result = first_value;
+	print(result);
+        for (int step = 0; step < NUM_STEPS; step++) {
+		result = 0.5 * (result + target/result);
+		print(result);
+        }
+
+        return result;
+
+}
+
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		cout << "Usage: ./square_root <natural_number>" << endl;
@@ -51,9 +65,11 @@ int main(int argc, char* argv[]) {
 	}
 	int target = stoi(argv[1]);
 	cout << "Square root of " << target << ":" << endl;
-	int first_value = find_first_value(target);
-	double result = square_root(first_value, target);
+	double result = square_root_by_binary_search(target);
 	cout << "Result: " << result << endl;
+
+	result = square_root_by_newton(target);
+        cout << "Result: " << result << endl;
 
 	return 0;
 }
