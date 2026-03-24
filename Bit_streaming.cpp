e658168529deea2af19aad3736329966a3643ed7
migu27 <migu27@gmail.com>
2026-03-24 05:49:49 +0100

    Add files via upload

--- Bit_streaming.cpp
+++ Bit_streaming.cpp
@@ -19,13 +19,14 @@ This looks trivial but it's not - just give it a try
 
 const int BUFFER_SZ = 12;
 uint8_t output_buffer[BUFFER_SZ];
-const int INPUT_SZ = 10;
+const int INPUT_SZ = 8;
 uint8_t input[INPUT_SZ];
 
+// ________________________________________ Helper functions
 void init() {
     // Init the input data with some recognizable simple pattern
     for (int i = 0; i < INPUT_SZ; ++i) {
-        input[i] = 0x15; //0x1F;
+        input[i] = 0x14; //0x15; //0x1F;
         uint8_t copy = input[i];
         for (int j = 0; j < 8; ++j) {
             // Print the value digit by digit
@@ -37,21 +38,9 @@ void init() {
         std::cout << " ";
     }
     std::cout << std::endl;
-    // input ==  00011111 00011111 00011111 00011111 00011111 00011111 00011111 00011111
+    
 
     memset(output_buffer, 0, sizeof(output_buffer));
-}
-
-uint8_t get_mask(uint8_t bits_to_read) {
-    /* Bits to read   |   mask
-       -----------------------
-            5         |   0x1F
-            4         |   0x0F 
-            3         |   0x07
-            2         |   0x03
-            1         |   0x01
-    */
-   return (pow(2, bits_to_read) - 1);
 }
 
 void print_output() {
@@ -69,8 +58,72 @@ void print_output() {
     std::cout << std::endl;
 }
 
-int main(int argc, char* argv[]) {
-    init();
+uint8_t get_mask(const uint8_t bits_to_read) {
+    /* Bits to read   |   mask
+       -----------------------
+            5         |   0x1F
+            4         |   0x0F 
+            3         |   0x07
+            2         |   0x03
+            1         |   0x01
+    */
+   return (pow(2, bits_to_read) - 1);
+}
+
+void get_digits(uint8_t number, uint8_t *result) {
+    // Returns an array of 5 elements where each contain a single bit 
+    // corresponding to the binary representation of "number"
+    // (memory has to be reserved by the caller). Example:
+    // number = 10100
+    // result[0] = 1, result[1] = 0, result[2] = 1, result[3] = 0, result[4] = 0
+    int i = 4; // The algorithm gets first the LSB, so we start the array by its last position
+    while (number > 0) {
+        result[i--] = number % 2;
+        number /= 2;
+    }
+}
+
+// _________________________________________ Helper functions (end)
+
+void algorithm_1() {
+    // This algorithm avoids the hell of shifts and masks, but requires more memory.
+    // It converts each original 5 bit value into an array of 5 separate numbers (one
+    // number per binary digit), into a bigger array. Then, it is just a matter of reading 
+    // elements (=digits) of this array in groups of 8 and converting back into a single  
+    // binary number, stored in a single element at the output.
+    // The additional memory needed is for the temporary buffer is 8 times the output size.
+    int i = 0;
+    uint8_t tmp_array[BUFFER_SZ * 8];
+    int output_idx = 0;
+    uint8_t a[5];
+
+    // Traverse the input stream. For each element (byte), discard the 3 significant bits 
+    // and convert the remaining 5 bits into 5 separate elements of a temporary buffer ("tmp_array") 
+    memset(tmp_array, 0, sizeof(tmp_array));
+    while (i < INPUT_SZ) {
+        get_digits(input[i++], a);
+        for (int j = 0; j < 5; ++j) {
+            tmp_array[output_idx++] = a[j];
+        }
+    }
+    // If input was: 00010100 00010100 ...
+    // Now we have: tmp_array[0] = 1, tmp_array[1] = 0, tmp_array[2] = 1, tmp_array[3] = 0, tmp_array[4] = 0,
+    //              tmp_array[5] = 1, tmp_array[6] = 0, tmp_array[7] = 1, tmp_array[8] = 0, tmp_array[9] = 0,...
+
+    // Convert from tmp_array into output
+    uint8_t val = 0;
+    for (int k = 0, out_idx = 0, exp = 7; k < (BUFFER_SZ * 8 - 1); ++k, --exp) {
+        val += tmp_array[k] * pow(2, exp);
+        if ((k > 0) && (k%8 == 0)) {
+            // Got a full byte: write it and reset counters
+            output_buffer[out_idx++] = val;
+            val = 0;
+            exp = 7;
+        }
+    }   
+}
+
+void algorithm_2() {
     // We need signed values for "w_bit" only, but I'm using "int" for all
     int r_bit = 4, r_byte = 0;
     int w_bit = 7, w_byte = 0; 
@@ -142,7 +195,13 @@ int main(int argc, char* argv[]) {
         }
     } 
     std::cout << "partial: " << (int) partial << ", w_byte: " << (int) w_byte << ", r_byte: " << (int) r_byte << std::endl;       
-        
+}
+
+int main(int argc, char* argv[]) {
+    init();
+
+    algorithm_1();
+    
     print_output();
     
     return 0;
